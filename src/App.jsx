4a5ec6ff1a917fd08abaa3dd8a93dc7f4c097955
jayAEY <106685702+jayAEY@users.noreply.github.com>
2024-02-28 13:51:47 -0800

    error handling added

--- src/App.jsx
+++ src/App.jsx
@@ -13,24 +13,32 @@ function App() {
   let inputDate = new Date(`${year},${month},${day}`);
 
   let result = new Date(currentDate - inputDate);
-  // console.log(currentDate, inputDate);
 
   let [returnYear, setReturnYear] = useState("- - ");
   let [returnMonth, setReturnMonth] = useState("- - ");
   let [returnDay, setReturnDay] = useState("- - ");
 
-  function calculate() {
+  function calculate(e) {
+    e.preventDefault();
+    // console.log(e);
     inputDate = new Date(`${year},${month},${day}`);
+    // console.log(inputDate);
     setReturnYear(result.getFullYear() - 1970);
     setReturnMonth(result.getMonth());
     setReturnDay(result.getDate());
-    console.log(returnDay, returnMonth, returnYear);
+    // console.log(returnDay, returnMonth, returnYear);
   }
 
   return (
     <main className="min-w-svw min-h-svh flex justify-center items-center font-poppins bg-[--light-grey]">
-      <div className="flex flex-col w-[320px] h-[440px] p-5 rounded-2xl rounded-br-[125px] bg-[white] lg:h-[450px] lg:w-[600px] lg:p-10 lg:rounded-br-[150px]">
-        <div className="flex w-full justify-between mt-4 lg:justify-start lg:gap-5 lg:mt-0">
+      <div className="flex flex-col w-[320px] h-[440px] p-5 rounded-2xl rounded-br-[125px] bg-[white] lg:h-[450px] lg:w-[600px] lg:p-10 lg:rounded-br-[150px] transition-all duration-200">
+        <form
+          id="date-input"
+          className="flex w-full justify-between mt-4 lg:justify-start lg:gap-5 lg:mt-0"
+          onSubmit={(e) => calculate(e)}
+          onInvalid={(e) => console.log(e.target.value)}
+        >
+          {/* <div className="flex w-full justify-between mt-4 lg:justify-start lg:gap-5 lg:mt-0"> */}
           <div className="flex flex-col lg:gap-2">
             <label
               className="uppercase font-bold text-xs text-[--smokey-grey] tracking-[0.2rem]"
@@ -40,12 +48,16 @@ function App() {
             </label>
             <input
               id="day"
+              form="date-input"
               type="number"
               placeholder="DD"
               className="w-[85px] h-11 p-2 text-black font-extrabold border-[--light-grey] border-2 rounded-md lg:w-[115px] lg:h-[55px] lg:text-[1.4rem] lg:font-bold lg:p-3"
               onChange={(e) => {
                 setDay(e.target.value);
               }}
+              min={1}
+              max={31}
+              required
             />
           </div>
 
@@ -58,12 +70,16 @@ function App() {
             </label>
             <input
               id="month"
+              form="date-input"
               type="number"
               placeholder="MM"
               className="w-[85px] h-11 p-2 text-black font-extrabold border-[--light-grey] border-2 rounded-md lg:w-[115px] lg:h-[55px] lg:text-[1.4rem] lg:font-bold lg:p-3"
               onChange={(e) => {
                 setMonth(e.target.value);
               }}
+              min={1}
+              max={12}
+              required
             />
           </div>
 
@@ -76,22 +92,35 @@ function App() {
             </label>
             <input
               id="year"
+              form="date-input"
               type="number"
               placeholder="YYYY"
               className="w-[85px] h-11 p-2 text-black font-extrabold border-[--light-grey] border-2 rounded-md lg:w-[115px] lg:h-[55px] lg:text-[1.4rem] lg:font-bold lg:p-3"
               onChange={(e) => {
                 setYear(e.target.value);
               }}
+              max={currentYear}
+              required
             />
           </div>
-        </div>
+        </form>
+        {/* </div> */}
 
         <div className="flex flex-col items-center w-full my-16 lg:my-8 lg:items-end">
           <span className="w-full h-0.5 bg-[--light-grey]" />
-          <div
-            className=" flex justify-center items-center rounded-[100px] bg-[--purple] -mt-8 w-[60px] h-[60px] transition-all hover:cursor-pointer hover:w-[64px] hover:h-[64px] hover:shadow-[0_0_18px_3px_rgba(133,77,255,0.5)] lg:scale-[115%]"
-            onClick={() => calculate()}
+          {/* <div */}
+          <button
+            type="submit"
+            form="date-input"
+            className=" flex justify-center items-center rounded-[100px] bg-[--purple] -mt-8 w-[60px] h-[60px] transition-all duration-300 hover:bg-black hover:cursor-pointer hover:w-[64px] hover:h-[64px] hover:shadow-[0_0_18px_3px_rgba(133,77,255,0.5)] lg:scale-[115%]"
+            // onClick={(e) => {
+            //   e.preventDefault();
+            //   calculate(e);
+            // }}
+
+            // onInvalid={(e) => console.log(e)}
           >
+            {/* > */}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="25"
@@ -106,7 +135,8 @@ function App() {
                 <path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44" />
               </g>
             </svg>
-          </div>
+          </button>
+          {/* </div> */}
         </div>
 
         <div className="mt-[180px] absolute lg:mt-[120px]">
