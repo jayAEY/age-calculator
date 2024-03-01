eecfc2947e7d3677422b96cf05ea787f5dee4169
jayAEY <106685702+jayAEY@users.noreply.github.com>
2024-03-01 12:37:47 -0800

    continued working on  error handling

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,6 @@ function App() {
   let currentMonth = new Date().getMonth();
   let currentDay = new Date().getDate();
 
-  // let [year, setYear] = useState(currentYear);
-  // let [month, setMonth] = useState(currentMonth + 1);
-  // let [day, setDay] = useState(currentDay);
-
   let [year, setYear] = useState();
   let [month, setMonth] = useState();
   let [day, setDay] = useState();
@@ -22,37 +18,101 @@ function App() {
   let [returnMonth, setReturnMonth] = useState("- - ");
   let [returnDay, setReturnDay] = useState("- - ");
 
+  let [dayError, setDayError] = useState("");
+  let [monthError, setMonthError] = useState("");
+  let [yearError, setYearError] = useState("");
+
   function calculate(e) {
     e.preventDefault();
-
     if (
+      !dayError &&
+      !monthError &&
+      !yearError &&
+      // checks if converted date is equal to inputs
       inputDate.getFullYear() == year &&
       inputDate.getMonth() + 1 == month &&
       inputDate.getDate() == day &&
+      //checks if in the past
       currentDate - inputDate > 0
     ) {
-      console.log("valid");
       setReturnYear(result.getFullYear() - 1970);
       setReturnMonth(result.getMonth());
       setReturnDay(result.getDate());
+      setDayError("");
+      setMonthError("");
+      setYearError("");
+    } else if (currentDate - inputDate < 0) {
+      setDayError("Must be in the past");
+      setMonthError(" ");
+      setYearError(" ");
     } else {
-      console.log("invalid");
+      setDayError("Must be a valid date");
     }
-    // console.log(returnDay, returnMonth, returnYear);
+  }
+
+  function handleDayError(value) {
+    value == value.trim();
+    if (value.length < 1) {
+      setDayError("This field is required");
+    } else if (
+      value.toLowerCase() != value.toUpperCase() ||
+      value < 1 ||
+      value > 31
+    ) {
+      setDayError("Must be a valid day");
+    } else {
+      setDayError("");
+      setDay(value);
+    }
+  }
+
+  function handleMonthError(value) {
+    value == value.trim();
+    if (value.length < 1) {
+      setMonthError("This field is required");
+    } else if (
+      value.toLowerCase() != value.toUpperCase() ||
+      value < 1 ||
+      value > 12
+    ) {
+      setMonthError("Must be a valid month");
+    } else {
+      setMonthError("");
+      setMonth(value);
+    }
+  }
+
+  function handleYearError(value) {
+    value == value.trim();
+    if (value.length < 1) {
+      setYearError("This field is required");
+    } else if (value.toLowerCase() != value.toUpperCase()) {
+      setYearError("Must be a valid year");
+    } else if (value > currentYear) {
+      setYearError("Must be in the past");
+    } else if (value < 100) {
+      setYearError("Please don't lie, you are not this old");
+    } else {
+      setYearError("");
+      setYear(value);
+    }
   }
 
   return (
     <main className="min-w-svw min-h-svh flex justify-center items-center font-poppins bg-[--light-grey]">
-      <div className="flex flex-col w-[320px] h-[440px] p-5 rounded-2xl rounded-br-[125px] bg-[white] lg:h-[490px] lg:w-[600px] lg:p-10 lg:rounded-br-[150px] transition-all duration-200">
+      <div className="flex flex-col w-[320px] h-[460px] p-5 rounded-2xl rounded-br-[115px] bg-[white] lg:h-[490px] lg:w-[600px] lg:p-10 lg:rounded-br-[150px] transition-all duration-200">
         <form
           id="date-input"
           className="flex w-full justify-between mt-4 lg:justify-start lg:gap-5 lg:mt-0"
-          onSubmit={(e) => calculate(e)}
-          onInvalid={(e) => console.log("invalid")}
+          onSubmit={(e) => {
+            calculate(e);
+          }}
         >
-          <div className="flex flex-col lg:gap-2">
+          <div className="flex flex-col gap-2">
             <label
-              className="uppercase font-bold text-xs text-[--smokey-grey] tracking-[0.2rem]"
+              className={`${
+                dayError ? "text-[--light-red]" : "text-[--smokey-grey]"
+              } uppercase font-bold text-xs tracking-[0.2rem]`}
               htmlFor="day"
             >
               Day
@@ -60,77 +120,96 @@ function App() {
             <input
               id="day"
               form="date-input"
-              type="number"
               placeholder="DD"
-              className="w-[85px] h-11 p-2 text-black font-extrabold border-[--light-grey] border-2 rounded-md lg:w-[125px] lg:h-[55px] lg:text-[1.4rem] lg:font-bold lg:p-3"
+              maxLength="2"
+              className={`${
+                dayError ? "border-[--light-red]" : "border-[--light-grey]"
+              } w-[85px] h-11 p-2 text-black font-extrabold border-2 rounded-md outline-none lg:w-[125px] lg:h-[55px] lg:text-[1.4rem] lg:font-bold lg:p-3`}
               onChange={(e) => {
-                setDay(e.target.value);
+                handleDayError(e.target.value);
               }}
-              min={1}
-              max={31}
-              required
             />
-            <span className="w-[70px] text-[0.8rem] mt-1 italic text-[--light-red] leading-3 lg:w-[115px] lg:text-[0.69rem]">
-              Must be a valid date
+            <span
+              id="day-error"
+              className={`${
+                dayError ? "visible" : "invisible"
+              } w-[70px] text-[0.7rem] h-4 mt-0 italic text-[--light-red] leading-3 lg:w-[125px] lg:text-[0.69rem]`}
+            >
+              {dayError}
             </span>
           </div>
 
-          <div className="flex flex-col lg:gap-2">
+          <div className="flex flex-col gap-2">
             <label
-              className="uppercase font-bold text-xs text-[--smokey-grey] tracking-[0.2rem]"
+              className={`${
+                monthError ? "text-[--light-red]" : "text-[--smokey-grey]"
+              } uppercase font-bold text-xs tracking-[0.2rem]`}
               htmlFor="month"
             >
               Month
             </label>
+
             <input
               id="month"
               form="date-input"
-              type="number"
               placeholder="MM"
-              className="w-[85px] h-11 p-2 text-black font-extrabold border-[--light-grey] border-2 rounded-md lg:w-[125px] lg:h-[55px] lg:text-[1.4rem] lg:font-bold lg:p-3"
+              maxLength="2"
+              className={`${
+                monthError ? "border-[--light-red]" : "border-[--light-grey]"
+              } w-[85px] h-11 p-2 text-black font-extrabold border-2 rounded-md outline-none lg:w-[125px] lg:h-[55px] lg:text-[1.4rem] lg:font-bold lg:p-3`}
               onChange={(e) => {
-                setMonth(e.target.value);
+                handleMonthError(e.target.value);
               }}
-              min={1}
-              max={12}
-              required
             />
-            <span className="w-[70px] text-[0.8rem] mt-1 italic text-[--light-red] leading-3 lg:w-[115px] lg:text-[0.69rem]">
-              This field is required
+            <span
+              id="month-error"
+              className={`${
+                monthError ? "visible" : "invisible"
+              } w-[70px] text-[0.7rem] h-4 mt-0 italic text-[--light-red] leading-3 lg:w-[125px] lg:text-[0.69rem]`}
+            >
+              {monthError}
             </span>
           </div>
 
-          <div className="flex flex-col lg:gap-2">
+          <div className="flex flex-col gap-2">
             <label
-              className="uppercase font-bold text-xs text-[--smokey-grey] tracking-[0.2rem]"
+              className={`${
+                yearError ? "text-[--light-red]" : "text-[--smokey-grey]"
+              } uppercase font-bold text-xs tracking-[0.2rem]`}
               htmlFor="year"
             >
               Year
             </label>
+
             <input
               id="year"
               form="date-input"
-              type="number"
               placeholder="YYYY"
-              className="w-[85px] h-11 p-2 text-black font-extrabold border-[--light-grey] border-2 rounded-md lg:w-[125px] lg:h-[55px] lg:text-[1.4rem] lg:font-bold lg:p-3"
+              maxLength="4"
+              className={`${
+                yearError ? "border-[--light-red]" : "border-[--light-grey]"
+              } w-[85px] h-11 p-2 text-black font-extrabold border-2 rounded-md outline-none lg:w-[125px] lg:h-[55px] lg:text-[1.4rem] lg:font-bold lg:p-3`}
               onChange={(e) => {
-                setYear(e.target.value);
+                handleYearError(e.target.value);
               }}
-              max={currentYear}
-              required
             />
-            <span className="w-[70px] text-[0.8rem] mt-1 italic text-[--light-red] leading-3 lg:w-[115px] lg:text-[0.69rem]">
-              This field is required
+            <span
+              id="year-error"
+              className={`${
+                yearError ? "visible" : "invisible"
+              } w-[70px] text-[0.7rem] h-4 mt-0 italic text-[--light-red] leading-3 lg:w-[125px] lg:text-[0.69rem]`}
+            >
+              {yearError}
             </span>
           </div>
         </form>
 
-        <div className="flex flex-col items-center w-full  my-11 lg:my-8 lg:items-end">
+        <div className="flex flex-col items-center w-full my-12 lg:my-8 lg:items-end">
           <span className="w-full h-0.5 bg-[--light-grey]" />
           <button
             type="submit"
             form="date-input"
-            className=" flex justify-center items-center rounded-[100px] bg-[--purple] -mt-8 w-[60px] h-[60px] transition-all duration-300 hover:bg-black hover:cursor-pointer hover:w-[64px] hover:h-[64px] hover:shadow-[0_0_18px_3px_rgba(133,77,255,0.5)] lg:scale-[115%]"
+            className="flex justify-center items-center rounded-[100px] bg-[--purple] -mt-8 w-[60px] h-[60px] transition-all duration-300 hover:bg-black hover:cursor-pointer hover:w-[64px] hover:h-[64px] hover:shadow-[0_0_18px_3px_rgba(133,77,255,0.5)] lg:scale-[115%]"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -149,7 +228,7 @@ function App() {
           </button>
         </div>
 
-        <div className="mt-[180px] absolute lg:mt-[155px]">
+        <div className="mt-[210px] absolute lg:mt-[155px]">
           <h1 className="text-[3.2rem] tracking-tight -mb-5 italic font-[800] lg:text-[4.5rem] lg:-mb-8">
             <span className="text-[--purple]">{returnYear}</span> years
           </h1>
